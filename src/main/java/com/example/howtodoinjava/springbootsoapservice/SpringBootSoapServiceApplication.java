d6a6efc1546ff150ccb99ba3d0834136a92c2ab2
dabbyp <dabbyp@naver.com>
2020-03-03 10:47:33 +0900

    Initial commit

---
+++ src/main/java/com/example/howtodoinjava/springbootsoapservice/SpringBootSoapServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.howtodoinjava.springbootsoapservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+public class SpringBootSoapServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSoapServiceApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/howtodoinjava/springbootsoapservice/StudentEndpoint.java
@@ -0,0 +1,31 @@
+package com.example.howtodoinjava.springbootsoapservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import com.howtodoinjava.xml.school.StudentDetailsRequest;
+import com.howtodoinjava.xml.school.StudentDetailsResponse;
+
+@Endpoint
+public class StudentEndpoint {
+	private static final String NAMESPACE_URI = "http://192.168.20.213:18080/xml/school";
+
+	private StudentRepository StudentRepository;
+
+	@Autowired
+	public StudentEndpoint(StudentRepository StudentRepository) {
+		this.StudentRepository = StudentRepository;
+	}
+
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "StudentDetailsRequest")
+	@ResponsePayload
+	public StudentDetailsResponse getStudent(@RequestPayload StudentDetailsRequest request) {
+		StudentDetailsResponse response = new StudentDetailsResponse();
+		response.setStudent(StudentRepository.findStudent(request.getName()));
+
+		return response;
+	}
+}
